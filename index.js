73095d074a89d5886195c677376b3cefad5b37db
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2020-08-20 19:35:44 -0700

    Update

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const express = require('express');
 var cors = require('cors');
 
+// Setup our Express App
 const app = express();
 app.use(cors());
 const PORT = 8080;
 
+// Define an endpoint
 app.get('/api/kanye', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
-
+  
+  // This is the object we want to return to our client
   const reply = {
     quote: "I have nothing profound to say!"
   };
-
+  
+  // Send the object as a JSON string
   return res.end(JSON.stringify(reply));
 });
 
+// Start listening for requests
 app.listen(PORT, () =>
   console.log(`App listening on port ${PORT}!`),
 );
