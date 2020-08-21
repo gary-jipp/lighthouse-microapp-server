6f6ad356f7e652144c3a42c6a6a2d92536c976e1
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2020-08-20 19:47:53 -0700

    Update

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
-const express = require('express');
-var cors = require('cors');
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
 
-// Setup our Express App
 const app = express();
 app.use(cors());
 const PORT = 8080;
+
+const quote = "One day I'm gonna run for President!";
+
+// Simple random number generator
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 // Define an endpoint
 app.get('/api/kanye', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   
-  // This is the object we want to return to our client
-  const reply = {
-    quote: "I have nothing profound to say!"
-  };
-  
-  // Send the object as a JSON string
-  return res.end(JSON.stringify(reply));
+  // Every so often inject our own message
+  const random = getRandomInt(1, 5);
+  if (random === 3) {
+    const reply = { quote };
+    return res.end(JSON.stringify(reply));
+  }
+
+
+  // Get data from the downstream service
+  const url = "https://api.kanye.rest";
+  axios.get(url)
+    .then(result => {
+      const reply = result.data;
+      return res.end(JSON.stringify(reply));
+    });
+
 });
 
 // Start listening for requests
-app.listen(PORT, () =>
-  console.log(`App listening on port ${PORT}!`),
-);
+app.listen(PORT, () => {
+  console.log("Running on port", PORT);
+});
